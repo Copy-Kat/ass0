fae6c72d2d4d763a50fabfc81782b3659a08ab5b
Copy-Kat <megumin2301@gmail.com>
2023-06-08 17:28:58 +0200

    add write to csv

--- flocking.py
+++ flocking.py
@@ -1,8 +1,11 @@
 from enum import Enum, auto
+from polars import select
+import pathlib
 
 import pygame as pg
 from pygame.math import Vector2
 from vi import Agent, Simulation
+import vi
 from vi.config import Config, dataclass, deserialize
 
 
@@ -13,11 +16,15 @@ class FlockingConfig(Config):
     cohesion_weight: float = 0.5
     separation_weight: float = 0.5
 
+    b_weight: float = 0.5
+    b_dist: int = 20
+
+    frame_count = 0
+
     delta_time: float = 3
 
     mass: int = 20
 
-    velocity: Vector2 = Vector2(1, 1)
 
     def weights(self) -> tuple[float, float, float]:
         return (self.alignment_weight, self.cohesion_weight, self.separation_weight)
@@ -28,22 +35,31 @@ def sum_vec2(list: list[Vector2]) -> Vector2:
         result += vec
     return result
 
+
+
 class Bird(Agent):
     config: FlockingConfig
 
-    max_v = Vector2(2, 2)
+    #i = 0
 
     def change_position(self):
+
         # Pac-man-style teleport to the other end of the screen when trying to escape
         self.there_is_no_escape()
+
         #YOUR CODE HERE -----------
-        # move is velocity 
-        #self.pos += self.move
+
         neighbors = list(self.in_proximity_accuracy().without_distance())
 
         neighbor_count = len(neighbors)
 
-        a, c, s = self.config.weights()
+        a, c, s = self.config.weights()        
+
+        prng = self.shared.prng_move
+
+        should_change_angle = prng.random()
+
+        deg = prng.uniform(-10, 10)
 
         if neighbor_count != 0:
 
@@ -51,58 +67,36 @@ class Bird(Agent):
 
             vec_arr = [agent.move for agent in neighbors]
 
-            #avg_x = sum(agent.move[0] for agent in close) / neighbor_count - self.move[0]
-            #avg_y = sum(agent.move[1] for agent in close) / neighbor_count - self.move[1]
-
             ave_v = sum_vec2(vec_arr) / neighbor_count
 
             alignment = (ave_v - self.move) * a
 
-            #sep_x = sum(self.pos[0] - agent.pos[0] for agent in close)
-            #sep_y = sum(self.pos[1] - agent.pos[1] for agent in close)
-
             seperation = Vector2(0, 0)
 
             for neighbor in neighbors:
-                seperation -= neighbor.pos - self.pos
+                seperation += self.pos - neighbor.pos
 
-            seperation *= s
-            
-            #avg_pos_x = sum(agent.pos[0] for agent in close) / neighbor_count
-            #avg_pos_y = sum(agent.pos[1] for agent in close) / neighbor_count
-
-            #f_c_x = avg_pos_x - self.pos[0]
-            #f_c_Y = avg_pos_y - self.pos[1]
-
-            #c_x = f_c_x - self.move[0]
-            #c_y = f_c_Y - self.move[1]
-
-            
+            seperation = seperation / neighbor_count * s
 
             center = sum_vec2(pos_arr) / neighbor_count
-            cohesion = (center - self.pos) * c
+            cohesion = ((center - self.pos) - self.move) * c
 
-            #cohesion = Vector2(c_x, c_y)
+            v4 = Vector2(0, 0)
 
+            for i in range(2):
+                if self.pos[i] < self.config.b_dist:
+                    v4[i] += self.config.b_weight
+                elif self.pos[i] > self.config.window.as_tuple()[i] - self.config.b_dist:
+                    v4[i] -= self.config.b_weight
 
-            #f_total_x = (a * avg_x + c * c_x + sep_x * s) / self.config.mass
-            #f_total_y = (a * avg_y + c * c_y + sep_y * s) / self.config.mass
-
-            #f_total = Vector2(f_total_x, f_total_y)
-
-            self.move += alignment + seperation + cohesion
+            self.move += (alignment + seperation + cohesion + v4)
             self.move = self.move.normalize()
 
-            #if self.move.magnitude() > 2:
-            #    self.move = self.move.normalize() * 2
-            self.pos += self.move
+        if 0.7 > should_change_angle:
+            self.move.rotate_ip(deg)
 
-        else:
+        self.pos += self.move
 
-            self.pos += self.move
-
-        #for agent, dist in close:
-        #    print(agent.move)
         #END CODE -----------------
 
 
@@ -110,7 +104,6 @@ class Selection(Enum):
     ALIGNMENT = auto()
     COHESION = auto()
     SEPARATION = auto()
-
 
 class FlockingLive(Simulation):
     selection: Selection = Selection.ALIGNMENT
@@ -139,8 +132,12 @@ class FlockingLive(Simulation):
                     self.selection = Selection.COHESION
                 elif event.key == pg.K_3:
                     self.selection = Selection.SEPARATION
+                elif event.key == pg.K_q:
+                    self._running = False
 
         a, c, s = self.config.weights()
+        self.config.frame_count += 1
+        print("Frame :", self.config.frame_count)
         print(f"A: {a:.1f} - C: {c:.1f} - S: {s:.1f}")
 
 config = FlockingConfig(
@@ -153,11 +150,8 @@ config = FlockingConfig(
 
 x, y = config.window.as_tuple()
 
-(
-    FlockingLive(config)
-    .batch_spawn_agents(100, Bird, images=["images/medium-bird.png"])
-    #.spawn_obstacle("images/bubble-full.png", x // 2, y // 2)
-    .run()
-)
+df = FlockingLive(config).batch_spawn_agents(100, Bird, images=["images/bird.png"]).run().snapshots
 
+print(df)
 
+df.write_csv("new_file.csv", separator=",")
